b6857f608fa5ea90eba2dd820cfa7708f3addd6a
marc lopes <marc_lopes2004@yahoo.fr>
2020-09-06 21:51:24 +0900

    added react emojione

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { emojify } from 'react-emojione';
 
 const Message = ({ message }) => {
   const messageTime = message.created_at.match(/(\d{2}:){2}\d{2}/)[0];
@@ -7,7 +8,7 @@ const Message = ({ message }) => {
       <div>
         <strong className="colored-name">{message.author}</strong> - {messageTime}
       </div>
-      <div>{message.content}</div>
+      <div>{emojify(message.content, { output: 'unicode' })}</div>
     </div>
   );
 };
